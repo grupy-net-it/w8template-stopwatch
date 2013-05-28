56b61af7b4c4bc067a3c386cbae1e0dd4c4ab00c
Kamil Cie�lak <keraxel@gmail.com>
2013-05-28 23:27:56 +0200

    Init commit

---
+++ Stopwatch/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Windows.UI.Xaml.Controls;
+
+namespace Stopwatch
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class MainPage : Stopwatch.Common.LayoutAwarePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="navigationParameter">The parameter value passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested.
+        /// </param>
+        /// <param name="pageState">A dictionary of state preserved by this page during an earlier
+        /// session.  This will be null the first time a page is visited.</param>
+        protected override void LoadState(object navigationParameter, Dictionary<string, object> pageState)
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+        }
+    }
+}
---
+++ template/TemplateImport/AppbarTop.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace TemplateImport
+{
+    /// <summary>
+    /// Interaction logic for AppbarTop.xaml
+    /// </summary>
+    public partial class AppbarTop : Page
+    {
+        public AppbarTop()
+        {
+            this.InitializeComponent();
+
+        }
+    }
+}
